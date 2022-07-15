b94ab08763c39bcf5930916d974ac2efb942d959
VolchokLV <volchoklv@gmail.com>
2022-07-15 16:53:18 -0700

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import logo from './img/Vapetasia_Logo_2_small.png';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+  return <nav className="nav">
+    <div className='site-logo'>
+      <a><img src={logo} alt="Vapetasia" /></a>
+    </div>
+    <div className='navbar-container'>
+        <div className='nav'>
+          <div className=''>
+            <a href="/home" className='active'>HOME</a>
+            <a href="/flavors">FLAVORS</a>
+            <a href="/disposables">DISPOSABLES</a>
+            <a href="/advocacy">ADVOCACY</a>
+            <a href="/blog">BLOG</a>
+            <a href="/aboutus">ABOUT US</a>
+            <a href="/contactus">CONTACT US</a>
+            <a href="/wholesale">WHOLESALE</a>
+            <a href="/storelocator">STORE LOCATOR</a>
+            <a href="/pact">PACT</a>
+          </div>
+        </div>
+      </div>
+  </nav>
 }
 
 export default Navbar

9b73c21674a6ec64bd9d6a8fefa097246be3dd07
VolchokLV <volchoklv@gmail.com>
2022-08-04 17:12:40 -0700

    attempting use state for responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+// import {React, useState}  from 'react';
+import React  from 'react';
 import logo from './img/Vapetasia_Logo_2_small.png';
-import { AiOutlineSearch } from 'react-icons/ai' 
+import { AiOutlineMenu, AiOutlineClose, AiOutlineSearch} from 'react-icons/ai';
 
 const Navbar = () => {
-  return <nav className="nav">
-    <div className='site-logo'>
-      <a><img src={logo} alt="Vapetasia" /></a>
-    </div>
+    // const [nav, setNav] = useState(false);
+  return (
+  
     <div className='navbar-container'>
-        <div className='nav'>
-          <div className=''>
-            <a href="/home" className='active'>HOME</a>
-            <a href="/flavors">FLAVORS</a>
-            <a href="/disposables">DISPOSABLES</a>
-            <a href="/advocacy">ADVOCACY</a>
-            <a href="/blog">BLOG</a>
-            <a href="/aboutus">ABOUT US</a>
-            <a href="/contactus">CONTACT US</a>
-            <a href="/wholesale">WHOLESALE</a>
-            <a href="/storelocator">STORE LOCATOR</a>
-            <a href="/pact">PACT</a>
-            <AiOutlineSearch size={25} style={{marginTop: '6px', color: 'white'}} />
-          </div>
-        </div>
-      </div>
-  </nav>
+        <nav className="nav">
+          <div className='site-logo'><a><img src={logo} alt="Vapetasia" /></a></div>          
+              <div className='nav'>
+                <div className=''>
+                  <a href="/home" className='active'>HOME</a>
+                  <a href="/flavors">FLAVORS</a>
+                  <a href="/disposables">DISPOSABLES</a>
+                  <a href="/advocacy">ADVOCACY</a>
+                  <a href="/blog">BLOG</a>
+                  <a href="/aboutus">ABOUT US</a>
+                  <a href="/contactus">CONTACT US</a>
+                  <a href="/wholesale">WHOLESALE</a>
+                  <a href="/storelocator">STORE LOCATOR</a>
+                  <a href="/pact">PACT</a>
+                  <AiOutlineSearch size={25} style={{marginTop: '6px', color: 'white'}} />
+                </div>
+              </div>
+          
+      </nav>
+      {/* <div onClick={()=> setNav(!nav)} className="mobile-nav">
+
+        {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30}/>}
+
+        <strong>Menu</strong> 
+
+      </div> */}
+    </div>
+  )  
 }
 
 export default Navbar

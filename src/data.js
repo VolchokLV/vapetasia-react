b1da2dd12ca9524abb709197f1bda92e07e7c8fe
VolchokLV <volchoklv@gmail.com>
2022-08-08 10:56:27 -0700

    fixed error after pull

--- src/data.js
+++ src/data.js
@@ -1,1 +1,18 @@
 // This is where I want to serialze the products
+
+
+// [
+//     {
+//         "product_name": "Killer Kustard",
+//         "main_img_src": "/img/USA_Killer_Kustard_100ml_3mg.png",
+//         "size": "30, 60, 100",
+//         "mg": "0,3,6,12",
+//         "hero_txt": "An enchanting mix of vanilla and creamy custard, reaching the perfect harmony of sweet and savory.",
+//         "sub_txt": "Our award-winning Killer Kustard is globally known for its unmatched vanilla custard flavor. Fair warning, this one is hard to put down!",
+//         "popup_txt": "Our award-winning Killer Kustard is globally known for its unmatched vanilla custard flavor. Fair warning, this one is hard to put down!",
+//         "popup_img_src": "/img/USA_Killer_Kustard_100ml_3mg.png",
+//         "buy_now1": "https://www.ejuices.com/products/vapetasia-killer-kustard",
+// 		"buy_now2": "https://vapesocietysupplies.com/products/killer-kustard-ejuice-100ml-vapetasia/",
+// 		"buy_now3": "https://www.elementvape.com/killer-kustard-vapetasia",
+//     },
+// ]	

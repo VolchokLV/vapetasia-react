7d58b43967cd7e0ac31c68ff54573a5cee4b0089
VolchokLV <volchoklv@gmail.com>
2022-10-05 13:35:08 -0700

    added wholesale registration pages and ifram to component

--- src/components/wholesaleRegistration/WholesaleRegistration.jsx
+++ src/components/wholesaleRegistration/WholesaleRegistration.jsx
@@ -6,6 +6,8 @@ const WholesaleRegistration = () => {
    // WHOLESALE REGISTRATION FORM GOES HERE
    <>
    
+
+<iframe width='610px' height='840px' src='https://crm.zoho.com/crm/WebFormServeServlet?rid=098768d23b55f35c975dfc75c7bb6de298c4148d4adcc96111ba53d38056574cgidb86e36298673cf220aba57ce4dda9dd4a05e06a500473b2f4862c9daf1a9e6f2'></iframe>
    </>
   )
 }

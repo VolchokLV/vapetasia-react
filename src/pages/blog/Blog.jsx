c3c542c57e1e0775530fb02431ed57597e0cf723
VolchokLV <volchoklv@gmail.com>
2022-10-07 17:09:00 -0700

    started blog page

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,9 +1,123 @@
 import React from 'react'
 import styles from './blog.css'
+import { Hero } from '../../components'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <>
+      <Hero heroClass="Blog" top={''} bottom={'BLOG'} paragraph={'Stay alert on the most up-to-date information regarding the vape industry and vape advocacy calls to action!'}></Hero>
+        <section className="vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-9872422 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="9872422" data-element_type="section">
+          <div className="vapetasia-container vapetasia-column-gap-default">
+            <div className="vapetasia-row">
+              <div className="vapetasia-column vapetasia-col-100 vapetasia-top-column vapetasia-element vapetasia-element-660bdd5" data-id="660bdd5" data-element_type="column">
+                <div className="vapetasia-column-wrap vapetasia-element-populated">
+                  <div className="vapetasia-widget-wrap">
+                    <div className="vapetasia-element vapetasia-element-7a977f0 vapetasia-widget-divider--view-line vapetasia-widget vapetasia-widget-divider" data-id="7a977f0" data-element_type="widget" data-widget_type="divider.default">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-f9b095e vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="f9b095e" data-element_type="section">
+          <div class="vapetasia-container vapetasia-column-gap-default">
+              <div class="vapetasia-row">
+                <div class="vapetasia-column vapetasia-col-100 vapetasia-top-column vapetasia-element vapetasia-element-8e6b34a" data-id="8e6b34a" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
+                    <div class="vapetasia-column-wrap vapetasia-element-populated">
+                      <div class="vapetasia-widget-wrap">
+                          <div class="vapetasia-element vapetasia-element-21aa2d1 vapetasia-widget vapetasia-widget-heading" data-id="21aa2d1" data-element_type="widget" data-widget_type="heading.default">
+                            <div class="vapetasia-widget-container">
+                                <h2 class="vapetasia-heading-title vapetasia-size-default">LATEST</h2>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </section>
+        <section class="vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-fa6f312 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="fa6f312" data-element_type="section">
+          <div class="vapetasia-container vapetasia-column-gap-default">
+              <div class="vapetasia-row">
+                <div class="vapetasia-column vapetasia-col-100 vapetasia-top-column vapetasia-element vapetasia-element-724ba89" data-id="724ba89" data-element_type="column">
+                    <div class="vapetasia-column-wrap vapetasia-element-populated">
+                      <div class="vapetasia-widget-wrap">
+                          <div class="vapetasia-element vapetasia-element-a6f26f7 vapetasia-widget-divider--view-line vapetasia-widget vapetasia-widget-divider" data-id="a6f26f7" data-element_type="widget" data-widget_type="divider.default">
+                            <div class="vapetasia-widget-container">
+                                <div class="vapetasia-divider">
+                                  <span class="vapetasia-divider-separator">
+                                  </span>
+                                </div>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </section>
+        <section class="vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-da24a34 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="da24a34" data-element_type="section">
+   <div class="vapetasia-container vapetasia-column-gap-default">
+      <div class="vapetasia-row">
+         <div class="vapetasia-column vapetasia-col-33 vapetasia-top-column vapetasia-element vapetasia-element-63044c7" data-id="63044c7" data-element_type="column">
+            <div class="vapetasia-column-wrap vapetasia-element-populated">
+               <div class="vapetasia-widget-wrap">
+                  <div class="vapetasia-element vapetasia-element-cf3292e vapetasia-widget vapetasia-widget-image" data-id="cf3292e" data-element_type="widget" data-widget_type="image.default">
+                     <div class="vapetasia-widget-container">
+                        <div class="vapetasia-image">
+                           <a href="https://vapetasia.com/the_ban_on_vape_mail" class="no-lightbox">
+                              <img width="360" height="360" src="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-Square.jpg" data-src="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-Square.jpg" class="attachment-large size-large lazyloaded" alt="Vape Mail Ban" data-srcset="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-Square.jpg 360w, https://cdn.vapetasia.com/wp-content/uploads/20210105005222/Vape-Mail-Ban-Square-300x300.jpg 300w, https://cdn.vapetasia.com/wp-content/uploads/20210105005222/Vape-Mail-Ban-Square-150x150.jpg 150w" sizes="(max-width: 360px) 100vw, 360px" srcset="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-Square.jpg 360w, https://cdn.vapetasia.com/wp-content/uploads/20210105005222/Vape-Mail-Ban-Square-300x300.jpg 300w, https://cdn.vapetasia.com/wp-content/uploads/20210105005222/Vape-Mail-Ban-Square-150x150.jpg 150w"></img>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="vapetasia-column vapetasia-col-66 vapetasia-top-column vapetasia-element vapetasia-element-9885b06" data-id="9885b06" data-element_type="column">
+            <div class="vapetasia-column-wrap vapetasia-element-populated">
+               <div class="vapetasia-widget-wrap">
+                  <div class="vapetasia-element vapetasia-element-34b6f8c vapetasia-widget vapetasia-widget-spacer" data-id="34b6f8c" data-element_type="widget" data-widget_type="spacer.default">
+                     <div class="vapetasia-widget-container">
+                        <div class="vapetasia-spacer">
+                           <div class="vapetasia-spacer-inner"></div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="vapetasia-element vapetasia-element-b15be63 vapetasia-widget vapetasia-widget-heading" data-id="b15be63" data-element_type="widget" data-widget_type="heading.default">
+                     <div class="vapetasia-widget-container">
+                        <h2 class="vapetasia-heading-title vapetasia-size-default">The Ban on Vape Mail</h2>
+                     </div>
+                  </div>
+                  <div class="vapetasia-element vapetasia-element-aff3605 vapetasia-widget vapetasia-widget-text-editor" data-id="aff3605" data-element_type="widget" data-widget_type="text-editor.default">
+                     <div class="vapetasia-widget-container">
+                        <div class="vapetasia-text-editor vapetasia-clearfix">
+                           <p><span data-preserver-spaces="true">New policies regarding vape related mail will go into place within the next 90-120 days with the signing of the massive&nbsp;</span><a href="https://www.washingtonpost.com/us-policy/2020/12/21/stimulus-congress/" target="_blank" rel="noopener">omnibus spending, coronavirus relief package</a><span data-preserver-spaces="true">.&nbsp;</span></p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="vapetasia-element vapetasia-element-c07aa5b vapetasia-align-center vapetasia-widget vapetasia-widget-button" data-id="c07aa5b" data-element_type="widget" data-widget_type="button.default">
+                     <div class="vapetasia-widget-container">
+                        <div class="vapetasia-button-wrapper">
+                           <a href="https://vapetasia.com/the_ban_on_vape_mail" class="vapetasia-button-link vapetasia-button vapetasia-size-sm" role="button">
+                           <span class="vapetasia-button-content-wrapper">
+                           <span class="vapetasia-button-text">READ MORE</span>
+                           </span>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+
+    </>
   )
 }
 

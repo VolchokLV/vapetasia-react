4921fb6d364cd0498451ecc136602990708868e3
Casey Henson <caseyahenson@gmail.com>
2022-10-19 16:40:16 -0700

    3-col images

--- src/pages/mediaRequest/MediaRequest.jsx
+++ src/pages/mediaRequest/MediaRequest.jsx
@@ -20,63 +20,6 @@ const MediaRequest = () => {
         </div>
       </div>
     </section>
-    {/* <section className="vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-be74176 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="be74176" data-element_type="section">
-      <div className="vapetasia-container vapetasia-column-gap-default">
-          <div className="vapetasia-row">
-            <div className="vapetasia-column vapetasia-col-100 vapetasia-top-column vapetasia-element vapetasia-element-28d1b9b" data-id="28d1b9b" data-element_type="column">
-                <div className="vapetasia-column-wrap vapetasia-element-populated">
-                  <div className="vapetasia-widget-wrap">
-                      <section className="vapetasia-section vapetasia-inner-section vapetasia-element vapetasia-element-bd8bae5 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="bd8bae5" data-element_type="section">
-                        <div className="vapetasia-container vapetasia-column-gap-default">
-                            <div className="vapetasia-row">
-                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-187b571" data-id="187b571" data-element_type="column">
-                                  <div className="vapetasia-column-wrap vapetasia-element-populated">
-                                    <div className="vapetasia-widget-wrap">
-                                        <div className="vapetasia-element vapetasia-element-ee0684a vapetasia-widget vapetasia-widget-image" data-id="ee0684a" data-element_type="widget" data-widget_type="image.default">
-                                          <div className="vapetasia-widget-container">
-                                              <div className="vapetasia-image">
-                                                <img width="748" height="632" src="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png" data-src="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png" className="attachment-large size-large lazyloaded" alt="" data-srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_3-300x253.png 300w" sizes="(max-width: 748px) 100vw, 748px" srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_3-300x253.png 300w"></img>
-                                              </div>
-                                          </div>
-                                        </div>
-                                    </div>
-                                  </div>
-                              </div>
-                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-9542ec6" data-id="9542ec6" data-element_type="column">
-                                  <div className="vapetasia-column-wrap vapetasia-element-populated">
-                                    <div className="vapetasia-widget-wrap">
-                                        <div className="vapetasia-element vapetasia-element-fe6884d vapetasia-widget vapetasia-widget-image" data-id="fe6884d" data-element_type="widget" data-widget_type="image.default">
-                                          <div className="vapetasia-widget-container">
-                                              <div className="vapetasia-image">
-                                                <img width="748" height="632" src="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png" data-src="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png" className="attachment-large size-large lazyloaded" alt="" data-srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_2-300x253.png 300w" sizes="(max-width: 748px) 100vw, 748px" srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_2-300x253.png 300w"></img>
-                                              </div>
-                                          </div>
-                                        </div>
-                                    </div>
-                                  </div>
-                              </div>
-                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-2dfc9d4" data-id="2dfc9d4" data-element_type="column">
-                                  <div className="vapetasia-column-wrap vapetasia-element-populated">
-                                    <div className="vapetasia-widget-wrap">
-                                        <div className="vapetasia-element vapetasia-element-e41d956 vapetasia-widget vapetasia-widget-image" data-id="e41d956" data-element_type="widget" data-widget_type="image.default">
-                                          <div className="vapetasia-widget-container">
-                                              <div className="vapetasia-image">
-                                                <img width="748" height="632" src="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png" data-src="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png" className="attachment-large size-large lazyloaded" alt="" data-srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_1-300x253.png 300w" sizes="(max-width: 748px) 100vw, 748px" srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_1-300x253.png 300w"></img>
-                                              </div>
-                                          </div>
-                                        </div>
-                                    </div>
-                                  </div>
-                              </div>
-                            </div>
-                        </div>
-                      </section>
-                  </div>
-                </div>
-            </div>
-          </div>
-      </div>
-    </section> */}
     <section className="vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-be74176 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="be74176" data-element_type="section">
       <div className="vapetasia-container vapetasia-column-gap-default">
           <div className="vapetasia-row">
@@ -86,44 +29,14 @@ const MediaRequest = () => {
                       <section className="vapetasia-section vapetasia-inner-section vapetasia-element vapetasia-element-bd8bae5 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default" data-id="bd8bae5" data-element_type="section">
                         <div className="vapetasia-container vapetasia-column-gap-default">
                             <div className="vapetasia-row">
-                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-187b571" data-id="187b571" data-element_type="column">
-                                  <div className="vapetasia-column-wrap vapetasia-element-populated">
-                                    <div className="vapetasia-widget-wrap">
-                                        <div className="vapetasia-element vapetasia-element-ee0684a vapetasia-widget vapetasia-widget-image" data-id="ee0684a" data-element_type="widget" data-widget_type="image.default">
-                                          <div className="vapetasia-widget-container">
-                                              <div className="vapetasia-image">
-                                                <img width="748" height="632" src="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png" data-src="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png" className="attachment-large size-large lazyloaded" alt="" data-srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_3-300x253.png 300w" sizes="(max-width: 748px) 100vw, 748px" srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_3-300x253.png 300w"></img>
-                                              </div>
-                                          </div>
-                                        </div>
-                                    </div>
-                                  </div>
+                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-187b571 padded-inner-image" data-id="187b571" data-element_type="column">
+                                  <img src="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png" className="attachment-large size-large" alt="Vapetasia Media Request" />
                               </div>
-                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-9542ec6" data-id="9542ec6" data-element_type="column">
-                                  <div className="vapetasia-column-wrap vapetasia-element-populated">
-                                    <div className="vapetasia-widget-wrap">
-                                        <div className="vapetasia-element vapetasia-element-fe6884d vapetasia-widget vapetasia-widget-image" data-id="fe6884d" data-element_type="widget" data-widget_type="image.default">
-                                          <div className="vapetasia-widget-container">
-                                              <div className="vapetasia-image">
-                                                <img width="748" height="632" src="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png" data-src="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png" className="attachment-large size-large lazyloaded" alt="" data-srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_2-300x253.png 300w" sizes="(max-width: 748px) 100vw, 748px" srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_2-300x253.png 300w"></img>
-                                              </div>
-                                          </div>
-                                        </div>
-                                    </div>
-                                  </div>
+                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-9542ec6 padded-inner-image" data-id="9542ec6" data-element_type="column">
+                                  <img src="https://cdn2.vapetasia.com/img/media-request/Media_Req_2.png" className="attachment-large size-large" alt="Vapetasia Media Request" />
                               </div>
-                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-2dfc9d4" data-id="2dfc9d4" data-element_type="column">
-                                  <div className="vapetasia-column-wrap vapetasia-element-populated">
-                                    <div className="vapetasia-widget-wrap">
-                                        <div className="vapetasia-element vapetasia-element-e41d956 vapetasia-widget vapetasia-widget-image" data-id="e41d956" data-element_type="widget" data-widget_type="image.default">
-                                          <div className="vapetasia-widget-container">
-                                              <div className="vapetasia-image">
-                                                <img width="748" height="632" src="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png" data-src="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png" className="attachment-large size-large lazyloaded" alt="" data-srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_1-300x253.png 300w" sizes="(max-width: 748px) 100vw, 748px" srcset="https://cdn2.vapetasia.com/img/media-request/Media_Req_1.png 748w, https://cdn2.vapetasia.com/img/media-request/Media_Req_1-300x253.png 300w"></img>
-                                              </div>
-                                          </div>
-                                        </div>
-                                    </div>
-                                  </div>
+                              <div className="vapetasia-column vapetasia-col-33 vapetasia-inner-column vapetasia-element vapetasia-element-2dfc9d4 padded-inner-image" data-id="2dfc9d4" data-element_type="column">
+                                  <img src="https://cdn2.vapetasia.com/img/media-request/Media_Req_3.png" className="attachment-large size-large" alt="Vapetasia Media Request" />
                               </div>
                             </div>
                         </div>

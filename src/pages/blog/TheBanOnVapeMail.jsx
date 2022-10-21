f163d912057f5c250fd5b99e4a06ae12e6e60439
VolchokLV <volchoklv@gmail.com>
2022-10-21 11:40:53 -0700

    blog updates

--- src/pages/blog/TheBanOnVapeMail.jsx
+++ src/pages/blog/TheBanOnVapeMail.jsx
@@ -1,52 +1,200 @@
-import React from 'react'
+import React from "react";
+import "./blog.css";
 
 const TheBanOnVapeMail = () => {
   return (
     <>
-    <iframe src={process.env.PUBLIC_URL + '/the-ban-on-vape-mail.html'} height="1000px" ></iframe>
-    {/* <section className="elementor-section elementor-top-section elementor-element elementor-element-bd55b00 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="bd55b00" data-element_type="section">
-   <div className="elementor-container elementor-column-gap-default">
-      <div className="elementor-row">
-         <div className="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-658b8a5" data-id="658b8a5" data-element_type="column">
-            <div className="elementor-column-wrap elementor-element-populated">
-               <div className="elementor-widget-wrap">
-                  <div className="elementor-element elementor-element-c4ebbe3 elementor-widget elementor-widget-image" data-id="c4ebbe3" data-element_type="widget" data-widget_type="image.default">
-                     <div className="elementor-widget-container">
-                        <div className="elementor-image">
-                           <img width="1024" height="418" src="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-1024x418.jpg" data-src="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-1024x418.jpg" className="attachment-large size-large lazyloaded" alt="Vape Mail Ban" data-srcset="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-1024x418.jpg 1024w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban-300x123.jpg 300w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban-768x314.jpg 768w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban-1536x627.jpg 1536w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban.jpg 2000w" sizes="(max-width: 1024px) 100vw, 1024px" srcset="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-1024x418.jpg 1024w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban-300x123.jpg 300w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban-768x314.jpg 768w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban-1536x627.jpg 1536w, https://cdn.vapetasia.com/wp-content/uploads/20210104201903/Vape-Mail-Ban.jpg 2000w"></img>
+      <div className="post-container">
+        {/* <iframe
+          src={process.env.PUBLIC_URL + "/the-ban-on-vape-mail.html"}
+          height="1000px"
+        ></iframe> */}
+        <section
+          className="vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-bd55b00 vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default"
+          data-id="bd55b00"
+          data-element_type="section"
+        >
+          <div className="post-container vapetasia-container vapetasia-column-gap-default">
+            <div className="vapetasia-row">
+              <div
+                className="vapetasia-column vapetasia-col-100 vapetasia-top-column vapetasia-element vapetasia-element-658b8a5"
+                data-id="658b8a5"
+                data-element_type="column"
+              >
+                <div className="vapetasia-column-wrap vapetasia-element-populated">
+                  <div className="vapetasia-widget-wrap">
+                    <div
+                      className="vapetasia-element vapetasia-element-c4ebbe3 vapetasia-widget vapetasia-widget-image"
+                      data-id="c4ebbe3"
+                      data-element_type="widget"
+                      data-widget_type="image.default"
+                    >
+                      <div className="vapetasia-widget-container">
+                        <div className="vapetasia-image">
+                          <img
+                            width="1024"
+                            height="418"
+                            src="https://cdn2.vapetasia.com/img/blog/Vape-Mail-Ban-1024x418.jpg"
+                            className="attachment-large size-large lazyloaded"
+                            alt="Vape Mail Ban"
+                          ></img>
                         </div>
-                     </div>
-                  </div>
-                  <div className="elementor-element elementor-element-fe110cb elementor-widget elementor-widget-text-editor" data-id="fe110cb" data-element_type="widget" data-widget_type="text-editor.default">
-                     <div className="elementor-widget-container">
-                        <div className="elementor-text-editor elementor-clearfix">
-                           <p><span style="color: #0e101a;background: transparent;margin-top: 0pt;margin-bottom: 0pt" data-preserver-spaces="true">New policies regarding vape related mail will go into place within the next 90-120 days with the signing of the massive&nbsp;</span><a style="background-image: initial;background-position: initial;background-size: initial;background-attachment: initial;margin-top: 0pt;margin-bottom: 0pt;color: #4a6ee0" href="https://www.washingtonpost.com/us-policy/2020/12/21/stimulus-congress/" target="_blank" rel="noopener">omnibus spending, coronavirus relief package</a><span style="color: #0e101a;background: transparent;margin-top: 0pt;margin-bottom: 0pt" data-preserver-spaces="true">. The 2021 omnibus bill includes the “Preventing Online Sales of E-Cigarettes to Children Act,” which would make it illegal for a consumer to purchase any e-cigarette product online and have it shipped to them. The vape mail ban will be in full effect around the end of the first quarter of 2021.&nbsp;</span></p>
-                           <p><span style="color: #0e101a;background: transparent;margin-top: 0pt;margin-bottom: 0pt" data-preserver-spaces="true">So what does this mean? There are two parts to this bill that we should look at. Unofficially the “Preventing Online Sales of E-Cigarettes to Children Act,” was referred to by many as the “vape mail ban.” As its unofficial name suggests, the bill will indeed require the US Postal Service to establish regulations within 120 days banning U.S. Mail delivery of vaping products. This would mean that consumers would not be able to purchase and ship vapor products through USPS. In addition, the bill also forces nicotine and cannabis vaping products to comply with the&nbsp;</span><a style="background-image: initial;background-position: initial;background-size: initial;background-attachment: initial;margin-top: 0pt;margin-bottom: 0pt;color: #4a6ee0" href="https://www.atf.gov/file/102101/download" target="_blank" rel="noopener">Prevent All Cigarette Trafficking (PACT) Act</a><span style="color: #0e101a;background: transparent;margin-top: 0pt;margin-bottom: 0pt" data-preserver-spaces="true">, which would impose stringent rules on online sellers. If the changes in policy are not followed within 90 days severe penalties will be issued, including up to three years in prison.</span></p>
-                           <p>Online retailers will be required to:</p>
-                           <ul>
-                              <li><span style="font-size: 10.5pt;line-height: 107%">Register with the U.S. Attorney General</span></li>
-                              <li><span style="font-size: 10.5pt;line-height: 107%">Verify age of customers using a commercially available database</span></li>
-                              <li><span style="font-size: 10.5pt;line-height: 107%">Use private shipping services that collect an adult signature at the point of delivery</span></li>
-                              <li><span style="font-size: 10.5pt;line-height: 107%">If selling in states that tax vaping products, sellers must register with the federal government and with the tobacco tax administrators of the states</span></li>
-                              <li><span style="font-size: 10.5pt;line-height: 107%">Collect all applicable local and state taxes, and affix any required tax stamps to the products sold</span></li>
-                              <li><span style="font-size: 10.5pt;line-height: 107%">Send each taxing state’s tax administrator a list of all transactions with customers in their state, including the names and addresses of each customer sold to, and the quantities and type of each product sold</span></li>
-                              <li>
-                                 <span style="font-size: 10.5pt;line-height: 107%">Maintain records for five years of any “delivery interrupted because the carrier or service determines or has reason to believe that the person ordering the delivery is in violation of the [PACT Act]”</span>
-                              </li>
-                           </ul>
-                           <p>Sadly, lawmakers push policies under the guise of “protecting the children,” at the cost of tax paying adults. Ironically, the government tax-spending bill seems counter intuitive with addition of this act. While suffocating a tax-generating industry, lawmakers risk the health of their adult citizens by making safer nicotine products more difficult to purchase than cigarettes. With the going of USPS options for online sellers, private delivery services are being pressured to follow suit. Fedex is one of the first private mail services that has announced that changes to their mailing policy regarding vapor products will change during the first quarter of the new year. 2021 is starting out with one of the biggest changes in our industry and introduces us to a new era where a vape mail ban is a reality.</p>
-                           <p><a href="https://vapetasia.com/blog/">Read More About Vape Industry News and Regulations</a></p>
+                      </div>
+                    </div>
+                    <div
+                      className="vapetasia-element vapetasia-element-fe110cb vapetasia-widget vapetasia-widget-text-editor"
+                      data-id="fe110cb"
+                      data-element_type="widget"
+                      data-widget_type="text-editor.default"
+                    >
+                      <div className="vapetasia-widget-container">
+                        <div className="vapetasia-text-editor vapetasia-clearfix">
+                          <p>
+                            <span>
+                              New policies regarding vape related mail will go
+                              into place within the next 90-120 days with the
+                            </span>
+                            <a
+                              href="https://www.washingtonpost.com/us-policy/2020/12/21/stimulus-congress/"
+                              target="_blank"
+                              rel="noopener"
+                            >
+                              signing of the massive omnibus spending,
+                              coronavirus relief package
+                            </a>
+                            <span>
+                              . The 2021 omnibus bill includes the “Preventing
+                              Online Sales of E-Cigarettes to Children Act,”
+                              which would make it illegal for a consumer to
+                              purchase any e-cigarette product online and have
+                              it shipped to them. The vape mail ban will be in
+                              full effect around the end of the first quarter of
+                              2021.
+                            </span>
+                          </p>
+                          <p>
+                            <span>
+                              So what does this mean? There are two parts to
+                              this bill that we should look at. Unofficially the
+                              “Preventing Online Sales of E-Cigarettes to
+                              Children Act,” was referred to by many as the
+                              “vape mail ban.” As its unofficial name suggests,
+                              the bill will indeed require the US Postal Service
+                              to establish regulations within 120 days banning
+                              U.S. Mail delivery of vaping products. This would
+                              mean that consumers would not be able to purchase
+                              and ship vapor products through USPS. In addition,
+                              the bill also forces nicotine and cannabis vaping
+                              products to comply with the&nbsp;
+                            </span>
+                            <a
+                              href="https://www.atf.gov/file/102101/download"
+                              target="_blank"
+                              rel="noopener"
+                            >
+                              Prevent All Cigarette Trafficking (PACT) Act
+                            </a>
+                            <span>
+                              , which would impose stringent rules on online
+                              sellers. If the changes in policy are not followed
+                              within 90 days severe penalties will be issued,
+                              including up to three years in prison.
+                            </span>
+                          </p>
+                          <p>Online retailers will be required to:</p>
+                          <ul>
+                            <li>
+                              <span>
+                                Register with the U.S. Attorney General
+                              </span>
+                            </li>
+                            <li>
+                              <span>
+                                Verify age of customers using a commercially
+                                available database
+                              </span>
+                            </li>
+                            <li>
+                              <span>
+                                Use private shipping services that collect an
+                                adult signature at the point of delivery
+                              </span>
+                            </li>
+                            <li>
+                              <span>
+                                If selling in states that tax vaping products,
+                                sellers must register with the federal
+                                government and with the tobacco tax
+                                administrators of the states
+                              </span>
+                            </li>
+                            <li>
+                              <span>
+                                Collect all applicable local and state taxes,
+                                and affix any required tax stamps to the
+                                products sold
+                              </span>
+                            </li>
+                            <li>
+                              <span>
+                                Send each taxing state’s tax administrator a
+                                list of all transactions with customers in their
+                                state, including the names and addresses of each
+                                customer sold to, and the quantities and type of
+                                each product sold
+                              </span>
+                            </li>
+                            <li>
+                              <span>
+                                Maintain records for five years of any “delivery
+                                interrupted because the carrier or service
+                                determines or has reason to believe that the
+                                person ordering the delivery is in violation of
+                                the [PACT Act]”
+                              </span>
+                            </li>
+                          </ul>
+                          <p>
+                            Sadly, lawmakers push policies under the guise of
+                            “protecting the children,” at the cost of tax paying
+                            adults. Ironically, the government tax-spending bill
+                            seems counter intuitive with addition of this act.
+                            While suffocating a tax-generating industry,
+                            lawmakers risk the health of their adult citizens by
+                            making safer nicotine products more difficult to
+                            purchase than cigarettes. With the going of USPS
+                            options for online sellers, private delivery
+                            services are being pressured to follow suit. Fedex
+                            is one of the first private mail services that has
+                            announced that changes to their mailing policy
+                            regarding vapor products will change during the
+                            first quarter of the new year. 2021 is starting out
+                            with one of the biggest changes in our industry and
+                            introduces us to a new era where a vape mail ban is
+                            a reality.
+                          </p>
+                          <p>
+                            <a
+                              href={
+                                process.env.REACT_APP_BASE_URL +
+                                "https://vapetasia.com/blog/"
+                              }
+                            >
+                              Read More About Vape Industry News and Regulations
+                            </a>
+                          </p>
                         </div>
-                     </div>
+                      </div>
+                    </div>
                   </div>
-               </div>
+                </div>
+              </div>
             </div>
-         </div>
+          </div>
+        </section>
       </div>
-   </div>
-</section> */}
-</>
-  )
-}
+    </>
+  );
+};
 
-export default TheBanOnVapeMail
+export default TheBanOnVapeMail;

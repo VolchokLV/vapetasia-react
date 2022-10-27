842e802373fc02fffd0840673763b1fba8d9f281
Casey Henson <caseyahenson@gmail.com>
2022-10-27 08:45:15 -0700

    additional logging

--- src/components/ageVerifyModal/AgeVerifyModal.jsx
+++ src/components/ageVerifyModal/AgeVerifyModal.jsx
@@ -20,6 +20,8 @@ const AgeVerifyModal = () => {
         var name = cname + "=";
         var decodedCookie = decodeURIComponent(document.cookie);
         var ca = decodedCookie.split(';');
+        console.log('cookies');
+        console.log(ca);
         for(var i = 0; i <ca.length; i++) {
             var c = ca[i];
             while (c.charAt(0) === ';') {
@@ -32,9 +34,11 @@ const AgeVerifyModal = () => {
         return "";
     }
     const setCookie = (cname, cvalue, exdays) => {
-        let date = new Date();
-        date.setTime(date.getTime()+(exdays*60*60*1000));
-        document.cookie = cname + " = " + cvalue + "; expires = " +date.toGMTString();
+        const domain = process.env.REACT_APP_BASE_URL.includes('http:') ? "" : "domain=.vapetasia.com;";
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;" + domain;
     }
     
     useEffect(() => {

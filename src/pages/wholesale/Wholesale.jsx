185824bf7692e12fce42a4cfb6eac991a297e27c
Casey Henson <caseyahenson@gmail.com>
2022-12-22 13:13:25 -0800

    need to fix initial display

--- src/pages/wholesale/Wholesale.jsx
+++ src/pages/wholesale/Wholesale.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useRef} from "react";
 import { Hero } from "../../components";
 import "./wholesale.css";
 import * as distributors from "../../data/distributors.json";
@@ -6,8 +6,9 @@ import * as distributors from "../../data/distributors.json";
 const Wholesale = () => {
 
   let checking = false;
-  const [coords, setCoords] = useState({lat: 0, long: 0})
+  const [userCoords, setUserCoords] = useState({lat: 0, long: 0})
   const [zip, setZip] = useState('')
+  const locations = useRef([])
 
   const getCoordsFromZip = () => {
     console.log('zip', zip);
@@ -18,7 +19,11 @@ const Wholesale = () => {
     .then((data) => {
       console.log(data);
       if ('lat' in data) {
-        setCoords({
+        setUserCoords({
+          lat: data.lat,
+          long: data.lon
+        })
+        sortByDistance({
           lat: data.lat,
           long: data.lon
         })
@@ -33,14 +38,14 @@ const Wholesale = () => {
     return deg * (Math.PI/180);
   }
 
-  const getDistanceFromUser = (lat, long) => {
+  const getDistanceFromUser = async (coords, loc) => {
     if (coords.lat === 0 || coords.long === 0) return 0;
     var R = 3960; // Radius of the earth in miles
-    var dLat = toRad(lat-coords.lat);
-    var dLon = toRad(long-coords.long); 
+    var dLat = toRad(loc.coords.lat-coords.lat);
+    var dLon = toRad(loc.coords.long-coords.long); 
     var a = 
       Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(toRad(coords.lat)) * Math.cos(toRad(lat)) * 
+      Math.cos(toRad(coords.lat)) * Math.cos(toRad(loc.coords.lat)) * 
       Math.sin(dLon/2) * Math.sin(dLon/2)
       ; 
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
@@ -48,10 +53,48 @@ const Wholesale = () => {
     return Math.round(d);
   }
 
+  const sortRandomly = async () => {
+      /*
+      Randomly order the distributors on initial load
+      */
+      let order = [];
+      distributors.default.map((d, idx) => {
+        order.push(idx);
+      });
+      for (let i = order.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [order[i], order[j]] = [order[j], order[i]];
+      }
+      /*
+      Create a new array mapping idx to link
+      */
+      let l = [];
+      order.map((idx) => {
+        l.push(distributors.default[idx]);
+      });
+      console.log('random', l);
+      locations.current = l;
+  }
+
+  const sortByDistance = async (userCoords) => {
+    let l = distributors.default;
+    let ld = await Promise.all(l.map(async loc => [loc, await getDistanceFromUser(userCoords, loc)]));
+    ld.sort((a, b) => {
+      return a[1] - b[1];
+    })
+    let lf = ld.map((loc) => {
+      loc[0]['distance'] = loc[1];
+      return loc[0];
+    })
+    locations.current = lf;
+  }
+
   const requestUserLocation = () => {
     if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCoords({lat: position.coords.latitude, long: position.coords.longitude});
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        setUserCoords({lat: position.coords.latitude, long: position.coords.longitude});
+        console.log('have coords');
+        await sortByDistance({lat: position.coords.latitude, long: position.coords.longitude});
       })
     } else {
       console.log('no access');
@@ -64,6 +107,9 @@ const Wholesale = () => {
      */
     if (!checking) {
       checking = true;
+
+      sortRandomly();
+
       requestUserLocation();
     }
   }, [])
@@ -207,146 +253,153 @@ const Wholesale = () => {
               </div>
             </div>
       </section>
-      {distributors.default.map((d, idx) => (
-        <section
-          key={'distributor_' + idx}
-          id="distributors-section"
-          className="distributors-section vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-2b06a3a vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default"
-          data-id="2b06a3a"
-          data-element_type="section"
-          data-settings='{"background_background":"classic"}'
-        >
-          <div className="vapetasia-container vapetasia-column-gap-default">
-            <div className="vapetasia-row">
-              <div
-                className="vapetasia-column vapetasia-col-16 vapetasia-top-column vapetasia-element vapetasia-element-0b73e85"
-                data-id="0b73e85"
-                data-element_type="column"
-              >
-                <div className="vapetasia-column-wrap vapetasia-element-populated">
-                  <div className="vapetasia-widget-wrap">
-                    <div
-                      className="vapetasia-element vapetasia-element-1219de6 vapetasia-widget vapetasia-widget-image"
-                      data-id="1219de6"
-                      data-element_type="widget"
-                      data-widget_type="image.default"
-                    >
-                      <div className="vapetasia-widget-container">
-                        <div className="vapetasia-image">
-                          <a href={d.name} className="no-lightbox">
-                            <img
-                              width="652"
-                              height="550"
-                              src={d.img_url}
-                              data-src={d.img_url}
-                              className="attachment-large size-large lazyloaded"
-                              alt={d.name}
-                            ></img>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="vapetasia-column vapetasia-col-66 vapetasia-top-column vapetasia-element vapetasia-element-6d3f1ae"
-                data-id="6d3f1ae"
-                data-element_type="column"
-              >
-                <div className="vapetasia-column-wrap vapetasia-element-populated">
-                  <div className="vapetasia-widget-wrap">
-                    <div
-                      className="vapetasia-element vapetasia-element-396dbc8 vapetasia-widget vapetasia-widget-heading"
-                      data-id="396dbc8"
-                      data-element_type="widget"
-                      data-widget_type="heading.default"
-                    >
-                      <div className="vapetasia-widget-container">
-                        <h2 className="vapetasia-heading-title vapetasia-size-default distributor-name">
-                          {d.name}
-                        </h2>
-                      </div>
-                    </div>
-                    <div
-                      className="vapetasia-element vapetasia-element-2b020cf vapetasia-widget vapetasia-widget-heading"
-                      data-id="2b020cf"
-                      data-element_type="widget"
-                      data-widget_type="heading.default"
-                    >
-                      <div className="vapetasia-widget-container">
-                        <h2 className="wholesale-heading-url vapetasia-size-default site-url">
-                          <a href={d.link}>{d.site_url}</a>
-                        </h2>
-                      </div>
-                    </div>
-                    <div
-                      className="vapetasia-element vapetasia-element-05ceac2 vapetasia-widget vapetasia-widget-heading"
-                      data-id="05ceac2"
-                      data-element_type="widget"
-                      data-widget_type="heading.default"
-                    >
-                      <div className="vapetasia-widget-container">
-                        <h2 className="wholesale-heading-phone vapetasia-size-default location-phone">
-                          {d.location_phone}
-                        </h2>
-                      </div>
-                    </div>
-                    <div
-                      className="vapetasia-element vapetasia-element-00c2da5 vapetasia-widget vapetasia-widget-text-editor"
-                      data-id="00c2da5"
-                      data-element_type="widget"
-                      data-widget_type="text-editor.default"
-                    >
-                      <div className="vapetasia-widget-container">
-                        <div className="vapetasia-text-editor vapetasia-clearfix distributor-address">
-                          <p>{d.address}</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="vapetasia-column vapetasia-col-16 vapetasia-top-column vapetasia-element vapetasia-element-35bf2a6"
-                data-id="35bf2a6"
-                data-element_type="column"
-              >
-                <div className="vapetasia-column-wrap vapetasia-element-populated">
-                  <div className="vapetasia-widget-wrap">
-                    <div
-                      className="vapetasia-element vapetasia-element-b7741f0 vapetasia-widget vapetasia-widget-image"
-                      data-id="b7741f0"
-                      data-element_type="widget"
-                      data-widget_type="image.default"
-                    >
-                      <div>{ 
-                        coords.lat === 0 || coords.long === 0 ?
-                          <div id="set-location-trigger">
-                            <span className="location-button" onClick={() => {requestUserLocation()}}>
-                              <i className="fa fa-map-marker"></i> enable location
-                              </span>
-                            <span>or set zip for distance</span>
-                          </div> :
-                          <div className="location-results">
-                            {getDistanceFromUser(parseFloat(d.coords.lat), parseFloat(d.coords.long))} miles
+      {
+        locations.current.length === 0 
+        ?
+        <div id="loading">
+          Loading Distributors
+        </div> 
+        :
+          locations.current.map((d, idx) => (
+            <section
+              key={'distributor_' + idx}
+              id="distributors-section"
+              className="distributors-section vapetasia-section vapetasia-top-section vapetasia-element vapetasia-element-2b06a3a vapetasia-section-boxed vapetasia-section-height-default vapetasia-section-height-default"
+              data-id="2b06a3a"
+              data-element_type="section"
+              data-settings='{"background_background":"classic"}'
+            >
+              <div className="vapetasia-container vapetasia-column-gap-default">
+                <div className="vapetasia-row">
+                  <div
+                    className="vapetasia-column vapetasia-col-16 vapetasia-top-column vapetasia-element vapetasia-element-0b73e85"
+                    data-id="0b73e85"
+                    data-element_type="column"
+                  >
+                    <div className="vapetasia-column-wrap vapetasia-element-populated">
+                      <div className="vapetasia-widget-wrap">
+                        <div
+                          className="vapetasia-element vapetasia-element-1219de6 vapetasia-widget vapetasia-widget-image"
+                          data-id="1219de6"
+                          data-element_type="widget"
+                          data-widget_type="image.default"
+                        >
+                          <div className="vapetasia-widget-container">
+                            <div className="vapetasia-image">
+                              <a href={d.name} className="no-lightbox">
+                                <img
+                                  width="652"
+                                  height="550"
+                                  src={d.img_url}
+                                  data-src={d.img_url}
+                                  className="attachment-large size-large lazyloaded"
+                                  alt={d.name}
+                                ></img>
+                              </a>
+                            </div>
                           </div>
-                        }</div>
-                      <div className="vapetasia-widget-container">
-                        <div className="vapetasia-button vapetasia-button-wholesale vapetasia-slide-button vapetasia-size-lg">
-                          <a href={d.contact}>CONTACT</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="vapetasia-column vapetasia-col-66 vapetasia-top-column vapetasia-element vapetasia-element-6d3f1ae"
+                    data-id="6d3f1ae"
+                    data-element_type="column"
+                  >
+                    <div className="vapetasia-column-wrap vapetasia-element-populated">
+                      <div className="vapetasia-widget-wrap">
+                        <div
+                          className="vapetasia-element vapetasia-element-396dbc8 vapetasia-widget vapetasia-widget-heading"
+                          data-id="396dbc8"
+                          data-element_type="widget"
+                          data-widget_type="heading.default"
+                        >
+                          <div className="vapetasia-widget-container">
+                            <h2 className="vapetasia-heading-title vapetasia-size-default distributor-name">
+                              {d.name}
+                            </h2>
+                          </div>
+                        </div>
+                        <div
+                          className="vapetasia-element vapetasia-element-2b020cf vapetasia-widget vapetasia-widget-heading"
+                          data-id="2b020cf"
+                          data-element_type="widget"
+                          data-widget_type="heading.default"
+                        >
+                          <div className="vapetasia-widget-container">
+                            <h2 className="wholesale-heading-url vapetasia-size-default site-url">
+                              <a href={d.link}>{d.site_url}</a>
+                            </h2>
+                          </div>
+                        </div>
+                        <div
+                          className="vapetasia-element vapetasia-element-05ceac2 vapetasia-widget vapetasia-widget-heading"
+                          data-id="05ceac2"
+                          data-element_type="widget"
+                          data-widget_type="heading.default"
+                        >
+                          <div className="vapetasia-widget-container">
+                            <h2 className="wholesale-heading-phone vapetasia-size-default location-phone">
+                              {d.location_phone}
+                            </h2>
+                          </div>
+                        </div>
+                        <div
+                          className="vapetasia-element vapetasia-element-00c2da5 vapetasia-widget vapetasia-widget-text-editor"
+                          data-id="00c2da5"
+                          data-element_type="widget"
+                          data-widget_type="text-editor.default"
+                        >
+                          <div className="vapetasia-widget-container">
+                            <div className="vapetasia-text-editor vapetasia-clearfix distributor-address">
+                              <p>{d.address}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    className="vapetasia-column vapetasia-col-16 vapetasia-top-column vapetasia-element vapetasia-element-35bf2a6"
+                    data-id="35bf2a6"
+                    data-element_type="column"
+                  >
+                    <div className="vapetasia-column-wrap vapetasia-element-populated">
+                      <div className="vapetasia-widget-wrap">
+                        <div
+                          className="vapetasia-element vapetasia-element-b7741f0 vapetasia-widget vapetasia-widget-image"
+                          data-id="b7741f0"
+                          data-element_type="widget"
+                          data-widget_type="image.default"
+                        >
+                          <div>{ 
+                            userCoords.lat === 0 || userCoords.long === 0 || !('distance' in d) ?
+                              <div id="set-location-trigger">
+                                <span className="location-button" onClick={() => {requestUserLocation()}}>
+                                  <i className="fa fa-map-marker"></i> enable location
+                                  </span>
+                                <span>or set zip for distance</span>
+                              </div> :
+                              <div className="location-results">
+                                {d.distance} miles
+                              </div>
+                            }</div>
+                          <div className="vapetasia-widget-container">
+                            <div className="vapetasia-button vapetasia-button-wholesale vapetasia-slide-button vapetasia-size-lg">
+                              <a href={d.contact}>CONTACT</a>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <hr />
-        </section>
-      ))}
-      ;
+              <hr />
+            </section>
+          ))
+        }
     </>
   );
 };
